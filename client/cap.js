7e4142b98bbeb7e0bff5bce4c1dedd83446bbecd
Joseph Tobin <jobin212@gmail.com>
2018-01-06 13:37:57 -0500

    hello world with react

--- client/cap.js
+++
@@ -1,6 +0,0 @@
-const capitalize = str => {
-    const lowStr = str.toLowerCase();
-    return lowStr[0].toUpperCase() + lowStr.slice(1);
-}
-
-module.exports = capitalize;
--- client/index.js
+++ client/index.js
@@ -1,6 +1,16 @@
-//node assumes .js extension 
-const capitalize = require('./cap');
+// import React from 'react'
+//cannot rely on user to use ES6 code, so we use webpack to convert to ES5
 
-console.log(capitalize('karley'));
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-console.log("hello")
+function App() {
+
+    return React.createElement(
+        "h1",
+        null,
+        "  Hello world "
+      );
+}
+
+ReactDOM.render(App(), document.getElementById('root'));
